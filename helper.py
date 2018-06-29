7130fc8ddf3d19aa4383ffeaaa9125e2062940ed
Ünver Can Ünlü <unvercanunlu@users.noreply.github.com>
2018-06-29 11:34:47 +0300

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 29 11:02:10 2018
+
+@author: Ünver Can Ünlü
+"""
+
+import csv
+from pathlib import Path
+
+# generate csv file using giving dictionaries
+def dictionaries_to_csv(output_file, dictionaries):
+    with open(file=output_file, mode='w', encoding='utf-8', newline='\n') as opened_file:
+        number_of_dictionaries = len(dictionaries)
+        if number_of_dictionaries > 0:
+            csv_writer = csv.writer(opened_file, delimiter=';')
+            # print header
+            header = [key.title() for key in list(dictionaries[0].keys())]
+            csv_writer.writerow(header)
+            # loop over dictionaries
+            for dictionary in dictionaries:
+                # print data
+                data = dictionary.values()
+                csv_writer.writerow(data)
+
+# check file exists in given path
+def check_file_exists(path):
+    possible_file = Path(path)
+    return possible_file.is_file()
