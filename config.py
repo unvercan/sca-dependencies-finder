739bead554f14d6ba0a5c2a74236c90d5b14c7cd
unver <unvercanunlu@gmail.com>
2025-02-11 22:18:51 +0100

    config is added.

---
+++ config.py
@@ -0,0 +1,43 @@
+# XML file extensions
+FILE_EXTENSIONS = [
+    "wsdl",
+    "xsd",
+    "xml",
+    "xsl",
+    "bpel",
+    "componentType",
+    "decs",
+    "dvm",
+    "jpr",
+    "edl",
+    "jca",
+    "jws",
+    "config",
+    "monitor",
+    "mwp",
+    "rules",
+    "sch",
+    "schema",
+    "table",
+    "offlinedb"
+]
+
+# XML elements
+ELEMENTS = [
+    "reference",
+    "component",
+    "service",
+    "import",
+    "schemaImport",
+    "schema-import",
+    "schema"
+]
+
+# XML attributes
+ATTRIBUTES = [
+    "location",
+    "wsdlLocation",
+    "schemaLocation",
+    "localPart",
+    "src"
+]
