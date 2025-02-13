75c27b144315a7ee4324a328a82f929181938796
unver <unvercanunlu@gmail.com>
2025-02-13 01:44:33 +0100

    helper updated.

--- helper.py
+++ helper.py
@@ -1,38 +1,31 @@
 import csv
+from pathlib import Path
 
-from model import Dependency, Result
+from model import Dependency
 
 
-def write_to_csv_file(output_file: str, dictionaries: list[dict] = None) -> None:
+def write_to_csv_file(file_path: Path, dictionaries: list[dict] = None) -> None:
     """generate csv file using giving dictionaries"""
     if not dictionaries:
         return
 
-    with open(file=output_file, mode="w", encoding="utf-8", newline="\n") as opened_file:
-        csv_writer = csv.writer(opened_file, dialect="excel", delimiter=";")
+    with open(file=file_path, mode="w", encoding="utf-8", newline="\n") as opened_file:
+        writer = csv.writer(opened_file, dialect="excel", delimiter=";")
         keys: list[str] = list(dictionaries[0].keys())
         header = [key.lower() for key in keys]
-        csv_writer.writerow(header)
+        writer.writerow(header)
         for dictionary in dictionaries:
-            data = [dictionary.get(key, "") for key in keys]
-            csv_writer.writerow(data)
+            row = [dictionary.get(key, "") for key in keys]
+            writer.writerow(row)
 
 
-def convert_dependency_to_dictionary(dependency: Dependency) -> dict:
+def convert_to_dictionary(dependency: Dependency) -> dict:
     """convert dependency object to dictionary"""
     return {
         "file": dependency.file,
         "element": dependency.element,
         "attribute": dependency.attribute,
         "path": dependency.path
-    }
-
-
-def convert_result_to_dictionary(result: Result) -> dict:
-    """convert result object to dictionary"""
-    return {
-        "type": result.category,
-        "dependencies": result.dependencies,
     }
 
 
